09cb7a64edaa2fa2b5e584d14d84b7ee9a5cf1d5
BYates1289 <benyates1289@live.com>
2021-11-07 16:48:10 +0000

    Account page with form validation.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -66,3 +66,46 @@ def login():
         flash("Login Unsuccessful. Please check email and password", "danger")
 
     return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    check = 0
+    if "username" in session:
+        check = 1
+    form = UpdateAccountForm()
+    users = mongo.db.users
+    user = users.find_one({"name": session["username"]})
+    if request.method == "POST":
+        if form.validate_on_submit():
+            if form.picture.data:
+                profile_image = form.picture.data
+                mongo.save_file(profile_image.filename, profile_image)
+                users.find_one_and_update(
+                    {"name": session["username"]},
+                    {"$set": {"profile_image_name": profile_image.filename}},
+                )
+
+            users.find_one_and_update(
+                {"name": session["username"]},
+                {"$set": {"name": form.username.data, "email": form.email.data}},
+            )
+            session.clear()
+            session["username"] = form.username.data
+            flash("Your account has been updated!", "success")
+            return redirect(url_for("account"))
+    form.username.data = user["name"]
+    form.email.data = user["email"]
+    try:
+        user["profile_image_name"]
+        image_file = url_for("file", filename=user["profile_image_name"])
+    except:
+        image_file = ""
+    return render_template(
+        "account.html",
+        title="Account",
+        user=user,
+        form=form,
+        profile_image=image_file,
+        check=check,
+    )
