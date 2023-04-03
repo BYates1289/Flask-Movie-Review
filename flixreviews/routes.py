948b044b79e396b125fcc84cb1b2b24c7d86ea62
BYates1289 <benyates1289@live.com>
2023-04-03 23:25:42 +0500

    Update routes.py

--- flixreviews/routes.py
+++ flixreviews/routes.py
@@ -1,17 +1,12 @@
 from datetime import datetime
+from flixreviews import app,bcrypt,db
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request,  session
+from flask_login import login_user, current_user, logout_user, login_required
+from flixreviews.forms import RegistrationForm, LoginForm,UpdateAccountForm, PostForm
+from flixreviews.models import User, Reviews
 import os
 import secrets
-from flixreviews import app, mongo
-from PIL import Image
-from flask import render_template, url_for, flash, redirect, request, \
-    abort, session
-from flask_login import login_user, current_user, logout_user, \
-    login_required
-from flixreviews.forms import RegistrationForm, LoginForm, \
-    UpdateAccountForm, PostForm
-import bcrypt
-from bson import ObjectId
-
 
 def ran(x):
     return range(int(x))
@@ -22,192 +17,157 @@ app.jinja_env.filters['ran'] = ran
 
 @app.route('/')
 def home():
-    check = 0
-    if 'username' in session:
-        check = 1
-    reviews = mongo.db['reviews'].find({})
-    return render_template('home.html', check=check, reviews=reviews)
+    reviews = Reviews.query.all()
+    return render_template('home.html', reviews=reviews)
 
 
-@app.route('/register', methods=['POST', 'GET'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = RegistrationForm()
-    if request.method == 'POST':
-        users = mongo.db.users
-        if form.validate_on_submit():
-            hashpass = bcrypt.hashpw(form.password.data.encode('utf-8'
-                    ), bcrypt.gensalt())
-            users.insert_one({'name': form.username.data,
-                         'password': hashpass,
-                         'email': form.email.data})
-
-            session['username'] = request.form['username']
-            return redirect(url_for('home'))
-
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if request.method == 'POST':
-        users = mongo.db.users
-        login_user = users.find_one({'name': form.username.data})
-        if login_user:
-            if bcrypt.hashpw(form.password.data.encode('utf-8'),
-                             login_user['password']) \
-                == login_user['password']:
-                session['username'] = form.username.data
-                return redirect(url_for('home'))
-        flash('Login Unsuccessful. Please check email and password',
-              'danger')
-
-    return render_template('login.html', title='Login', form=form)
-
-
-@app.route('/account', methods=['GET', 'POST'])
-def account():
-    check = 0
-    if 'username' in session:
-        check = 1
-    form = UpdateAccountForm()
-    users = mongo.db.users
-    user = users.find_one({'name': session['username']})
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            if form.picture.data:
-                profile_image = form.picture.data
-                mongo.save_file(profile_image.filename, profile_image)
-                users.find_one_and_update({'name': session['username'
-                        ]},
-                        {'$set': {'profile_image_name': profile_image.filename}})
-
-            users.find_one_and_update({'name': session['username']},
-                    {'$set': {'name': form.username.data,
-                    'email': form.email.data}})
-            session.clear()
-            session['username'] = form.username.data
-            flash('Your account has been updated!', 'success')
-            return redirect(url_for('account'))
-    form.username.data = user['name']
-    form.email.data = user['email']
-    try:
-        user['profile_image_name']
-        image_file = url_for('file', filename=user['profile_image_name'
-                             ])
-    except:
-        image_file = ''
-    return render_template(
-        'account.html',
-        title='Account',
-        user=user,
-        form=form,
-        profile_image=image_file,
-        check=check,
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
         )
+        user = User(
+            username=form.username.data,  password=hashed_password, 
+            email=form.email.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
 
 
-@app.route('/logout')
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check email and password", "danger")
+    return render_template("login.html", title="Login", form=form)
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, "static/profile-images", picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route("/account", methods=["GET", "POST"])
+@login_required
+def account():
+    form = UpdateAccountForm() 
+
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", "success")
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for("static", filename="profile-images/" + current_user.image_file)
+    return render_template(
+        "account.html", title="Account", image_file=image_file, form=form
+    )
+
+@app.route("/logout")
 def logout():
-    session.clear()
-    return redirect(url_for('home'))
-
-
-@app.route('/file/<filename>')
-def file(filename):
-    return mongo.send_file(filename)
-
+    logout_user()
+    return redirect(url_for("home"))
 
 @app.route('/give-review', methods=['GET', 'POST'])
 def new_review():
-    check = 0
-    if 'username' in session:
-        check = 1
     form = PostForm()
-    reviews = mongo.db.reviews
     if form.validate_on_submit():
-        reviews.insert_one({
-            'title': form.title.data,
-            'link': form.link.data,
-            'review': form.review.data,
-            'content': form.content.data,
-            'star': request.form['star'],
-            'author': session['username'],
-            'created_at': str(datetime.now().date()),
-            })
-
-        flash('Your post has been created!', 'success')
+        review = Reviews(
+            title=form.title.data,
+            link=form.link.data,
+            review=form.review.data,
+            content=form.content.data,
+            star=request.form['star'],
+            author= current_user,
+        )
+        db.session.add(review)
+        db.session.commit()
+        flash('Your Review has been Submitted!', 'success')
         return redirect(url_for('home'))
     return render_template('create_review.html', title='New Review',
-                           form=form, check=check)
+                           form=form)
 
 
 @app.route('/reviews')
 def reviews():
-    check = 0
-    if 'username' in session:
-        check = 1
-        user = session['username']
-    reviews = mongo.db['reviews'].find({})
-    return render_template('reviews.html', reviews=reviews,
-                           check=check, user=user)
+    reviews = Reviews.query.all()
+    return render_template('reviews.html', reviews=reviews)
 
 
 @app.route('/review_delete/<string:review_id>', methods=['Post'])
 def review_delete(review_id):
-    reviews = mongo.db.reviews
-    delete = reviews.delete_one({'_id': ObjectId(review_id)})
-    flash('Your review has been deleted!', 'success')
-    return redirect(url_for('reviews'))
+    dlt = Reviews.query.get_or_404(review_id)
+    if dlt.author != current_user:
+        return redirect(url_for('logout'))
+    else:
+        db.session.delete(dlt)
+        db.session.commit()
+        flash('Your review has been deleted!', 'success')
+        return redirect(url_for('reviews'))
 
 
 @app.route('/review_show/<string:review_id>', methods=['GET'])
 def review_show(review_id):
-    check = 0
-    if 'username' in session:
-        check = 1
-    reviews = mongo.db.reviews
-    review = reviews.find_one({'_id': ObjectId(review_id)})
-    return render_template('single_review.html', review=review,
-                           check=check)
+    review = Reviews.query.filter_by(id=review_id).first()
+    return render_template('single_review.html', review=review)
 
 
 @app.route("/review_edit/<string:review_id>", methods=["GET", "POST"])
 def review_edit(review_id):
-    check = 0
-    if "username" in session:
-        check = 1
-    reviews = mongo.db.reviews
-    review = reviews.find_one({"_id": ObjectId(review_id)})
+    review = Reviews.query.filter_by(id=review_id).first()
     form = PostForm()
+
     if request.method == "POST":
         if form.validate_on_submit():
+            review.star = request.form["star"]
+            review.link = form.link.data
+            review.review = form.review.data
+            review.content = form.content.data
+            review.title = form.title.data
 
-            reviews.update_one(
-                {"_id": ObjectId(review_id)},
-                {
-                    "$set": {
-                        "title": form.title.data,
-                        "link": form.link.data,
-                        "review": form.review.data,
-                        "content": form.content.data,
-                        "star": request.form["star"],
-                        "author": session["username"],
-                        "created_at": str(datetime.now().date()),
-                    }
-                },
-            )
+            db.session.add(review)
+            db.session.commit()
             flash("Your review has been updated!", "success")
             return redirect(url_for("reviews"))
+        return render_template("register.html", title="Register", form=form)
     elif request.method == "GET":
-        form.title.data = review["title"]
-        form.link.data = review["link"]
-        form.review.data = review["review"]
-        form.content.data = review["content"]
+        form.title.data = review.title
+        form.link.data = review.link
+        form.review.data = review.review
+        form.content.data = review.content
         return render_template(
             "edit_review.html",
             title="New Review",
             form=form,
-            check=check,
-            star=review["star"],
+            star=review.star,
         )
